93e81dfec618f787cf84db2abc053e17bb6ee5b0
Obde Willy <obde_willy@hotmail.com>
2023-09-27 09:24:14 -0300

    First commit

---
+++ TesteWebView/TesteWebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteWebViewApp.swift
+//  TesteWebView
+//
+//  Created by Obde Willy on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TesteWebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
